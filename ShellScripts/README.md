8ab4b916f6d7cbd031178229b7d71d9b2059f832
Ashish Rawat <Ashish.rawat417@gmail.com>
2020-10-10 02:39:30 +1100

    Remove Shell Commands

--- ShellScripts/README.md
+++
@@ -1,2 +0,0 @@
-# server_scripts
-This small shell scripts helps to automate system feed and internal hardware system to update dns and nameservers config files
--- ShellScripts/clearcache.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ssh 192.168.245.24 'sync; echo 3 > /proc/sys/vm/drop_caches';
-ssh 192.168.245.27 'sync; echo 3 > /proc/sys/vm/drop_caches';
-ssh 192.168.245.37 'sync; echo 3 > /proc/sys/vm/drop_caches';
-sync; echo 3 > /proc/sys/vm/drop_caches
